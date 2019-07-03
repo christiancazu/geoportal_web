02c979c3973fd2ee2363e9aa7baa5396a02d477f
mariley <mariley2018@gmail.com>
2019-07-03 10:43:01 -0500

    recover password

--- pages/recoverPassword.vue
+++ pages/recoverPassword.vue
@@ -1,52 +1,50 @@
 <template>
-  <div>
-    <el-container direction="vertical">
-      <el-row class="content-center">
-        <el-col
-          :xs="24"
-          :sm="12"
-          :md="8"
-          :lg="6"
-        >
-          <el-card shadow="always">
-            <div slot="header">
-              <label>Recuperar Contraseña</label>
-            </div>
-            <div>
-              <el-form
-                ref="form"
-                label-position="top"
-                status-icon
-                :model="form"
-                :rules="rules2"
-                label-width="120px"
-                class="demo-ruleForm"
+  <el-container direction="vertical">
+    <el-row class="content-center">
+      <el-col
+        :xs="24"
+        :sm="12"
+        :md="8"
+        :lg="6"
+      >
+        <el-card shadow="always">
+          <div slot="header">
+            <label>Recuperar Contraseña</label>
+          </div>
+          <div>
+            <el-form
+              ref="form"
+              label-position="top"
+              status-icon
+              :model="form"
+              :rules="rules2"
+              label-width="120px"
+              class="demo-ruleForm"
+            >
+              <el-form-item
+                prop="email"
+                label="Correo Electrónico"
+                :rules="rules2.email"
               >
-                <el-form-item
-                  prop="email"
-                  label="Correo Electrónico"
-                  :rules="rules2.email"
+                <el-input
+                  v-model="form.email"
+                  type="email"
+                />
+              </el-form-item>
+              <el-form-item class="text-xs-center pt-3 mb-0">
+                <el-button
+                  type="primary"
+                  @click="submitForm"
                 >
-                  <el-input
-                    v-model="form.email"
-                    type="email"
-                  />
-                </el-form-item>
-                <el-form-item class="text-xs-center pt-3 mb-0">
-                  <el-button
-                    type="primary"
-                    @click="submitForm"
-                  >
-                    Recuperar Contraseña
-                  </el-button>
-                </el-form-item>
-              </el-form>
-            </div>
-          </el-card>
-        </el-col>
-      </el-row>
-    </el-container>
-  </div>
+                  Recuperar Contraseña
+                </el-button>
+              </el-form-item>
+            </el-form>
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
+  </el-container>
 </template>
 
 <script>
