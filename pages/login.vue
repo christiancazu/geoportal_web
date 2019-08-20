67cf88b04173ae78248c1eb8b6ea1f07d7f3f8df
mariley <mariley2018@gmail.com>
2019-08-20 18:39:36 -0500

    advanced login admin

--- pages/login.vue
+++ pages/login.vue
@@ -1,68 +1,137 @@
 <template>
-  <div
-    class="theme-purple-pink p-4"
-    style="height: 100%"
-  >
-    <el-container
-      class="container-fluid"
-      style="justify-content: center"
+  <!-- <el-container
+    class="container-fluid"
+    style="justify-content: center"
+  > -->
+  <el-row :gutter="10" class="content-center pt-5">
+    <el-col
+      :xs='24'
+      :md="14"
+      :sm="16"
+      :lg="10"
     >
-      <el-card shadow="always" class="ma-4">
-        <div
-          class="text-xs-center"
-          slot="header"
+      <!-- <el-card
+        shadow="always"
+        class="ma-4"
+      > -->
+      <el-row
+        :gutter="2"
+        class="el-card is-always-shadow"
+      >
+        <el-col
+          :xs="24"
+          :md="12"
+          :sm="12"
+          :lg="12"
+          class="text-xs-center br"
         >
-          <label>Iniciar Sesión</label>
-        </div>
-        <div>
-          <el-form
-            ref="formLogin"
-            label-position="top"
-            status-icon
-            :model="form"
-            :rules="rules"
-            label-width="120px"
-            class="demo-ruleForm"
-            @submit.native.prevent="submitForm"
+          <el-card
+            shadow="never"
+            class="border-none"
           >
-            <el-form-item
-              prop="email"
-              label="Nombre de Usuario"
-              :rules="rules.email"
+            <div
+              slot="header"
+              class="clearfix"
             >
-              <el-input
-                :disabled="processingForm"
-                v-model="form.email"
-                type="text"
-              />
-            </el-form-item>
-            <el-form-item
-              label="Contraseña"
-              prop="password"
-            >
-              <el-input
-                :disabled="processingForm"
-                v-model="form.password"
-                type="password"
-                autocomplete="off"
-                :rules="rules.password"
-              />
-            </el-form-item>
-            <el-form-item class="text-xs-center mb-0">
-              <el-button
-                :loading="processingForm"
+              <p class="ma-0 font-weight-bold">PORTAL DE ADMINISTRADORES</p>
+            </div>
+            <p class="font-weight-bold"></p>
+            <el-image
+              style="width: 150px; height: 150px"
+              src="image/logo_escudo.png"
+              fit="contain"
+            ></el-image>
+            <p>¿Aún no tienes cuenta? <el-link
                 type="primary"
-                native-type="submit"
-                @click.prevent="submitForm"
+                class="font-weight-bold"
+              >Regístrese</el-link>
+            </p>
+            <small>
+              <el-link type="primary">Ir pagina principal</el-link>
+            </small>
+
+          </el-card>
+        </el-col>
+        <el-col
+          :xs="24"
+          :md="12"
+          :sm="12"
+          :lg="12"
+          class="bt"
+        >
+          <el-card
+            shadow="never"
+            class="border-none"
+          >
+            <div
+              class="text-xs-center p"
+              slot="header"
+            >
+              <label>INICIAR SESIÓN</label>
+            </div>
+            <el-form
+              ref="formLogin"
+              label-position="top"
+              status-icon
+              :model="form"
+              :rules="rules"
+              label-width="120px"
+              class="demo-ruleForm"
+              @submit.native.prevent="submitForm"
+            >
+              <el-form-item
+                prop="email"
+                label="Nombre de Usuario"
+                :rules="rules.email"
               >
-                INGRESAR
-              </el-button>
-            </el-form-item>
-          </el-form>
-        </div>
-      </el-card>
-    </el-container>
-  </div>
+                <el-input
+                  :disabled="processingForm"
+                  v-model="form.email"
+                  type="text"
+                />
+              </el-form-item>
+              <el-form-item
+                label="Contraseña"
+                prop="password"
+              >
+                <el-input
+                  :disabled="processingForm"
+                  v-model="form.password"
+                  type="password"
+                  autocomplete="off"
+                  :rules="rules.password"
+                />
+              </el-form-item>
+              <p class="text-xs-right">
+                <el-link type="primary">Olvidaste tu contraseña</el-link>
+              </p>
+              <el-form-item class="text-xs-center mb-0">
+                <el-button
+                  :loading="processingForm"
+                  type="primary"
+                  size="small"
+                  native-type="submit"
+                  @click.prevent="submitForm"
+                >
+                  INGRESAR
+                </el-button>
+              </el-form-item>
+
+            </el-form>
+            <div
+              class="text-xs-center"
+              slot="footer"
+            >
+              <label>INICIAR SESIÓN</label>
+            </div>
+          </el-card>
+        </el-col>
+      </el-row>
+      <!-- </el-card> -->
+    </el-col>
+  </el-row>
+
+  <!-- </el-container> -->
 </template>
 
 <script>
