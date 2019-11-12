dc238559538ec89cd0d5a5bb3d195e3cd39c23a3
mariley <mariley2018@gmail.com>
2019-11-12 18:15:44 -0500

    list request pending

--- pages/managementUsers/pendingUsers/index.vue
+++ pages/managementUsers/pendingUsers/index.vue
@@ -5,17 +5,15 @@
   :filter-criteria-props="filterCriteriaProps"
   :message-toast="messageToast"
 >
-  <template
-    v-slot:page-table
-  >
+  <template v-slot:page-table>
     <el-table-column
       label="Institución"
       prop="institute"
     />
     <el-table-column label="Nombres y Apellidos">
-      <template
-        slot-scope="scope"
-      >{{ `${scope.row.name} ${scope.row.lastName} ${scope.row.lastNameAditional}` }}</template>
+      <template slot-scope="scope">
+        {{ `${scope.row.name} ${scope.row.lastName} ${scope.row.lastNameAditional}` }}
+      </template>
     </el-table-column>
     <el-table-column
       label="Correo Electrónico"
@@ -28,12 +26,12 @@
     >
       <template slot-scope="scope">
         <el-tooltip
-          content="Editar"
+          content="Ver solicitud"
           placement="bottom"
         >
           <el-button
             circle
-            icon="el-icon-edit"
+            icon="el-icon-view"
             size="small"
             type="primary"
             @click="onLoadModalApproveItem(scope.row)"
@@ -48,12 +46,8 @@
 <script>
 
 import pageActionsMixin from '@/mixins/pageActionsMixin'
-import { SET_CURRENT_PAGE_ON_TABLE } from '@/types/mutation-types'
 import { SET_ITEM_CONTEXT } from '@/types/mutation-types'
-import { ROWS_PER_PAGE_ON_TABLE } from '@/config/constants'
 
-
-import { mapActions, mapState } from 'vuex'
 export default {
   mixins: [pageActionsMixin],
   data () {
@@ -81,68 +75,15 @@ export default {
     }
   },
 
-  computed: {
-    ...mapState({
-      loadingPendingRequests: state => state.userRequests.loadingPendingRequests
-    }),
-  },
-
   methods: {
-    ...mapActions({
-      getPendingRequests: 'userRequests/getPendingRequests',
-      replaceCurrentPendingRequest: 'userRequests/replaceCurrentPendingRequest',
-    }),
-
-    onChangePageSize: function (pagesize) {
-      this.pagesize = pagesize
-    },
-
-    deleteUser (item) {
-      new Promise((resolve, reject) => {
-        this.$userAPI.delete({ id: item.itemSelected.id })
-          .then(response => {
-            resolve(response)
-            this.getUsers()
-          }).catch(error => reject(error))
-      })
-    },
-
     onLoadModalApproveItem (item) {
-      console.log('item', item)
-      this.$store.commit(`${this.modalMain.storeBase}/${SET_ITEM_CONTEXT}`, {itemContext: item })
+      this.$store.commit(`${this.modalMain.storeBase}/${SET_ITEM_CONTEXT}`, { itemContext: item })
 
       this.$store.dispatch('modalsVisibilities/openModal', {
         modalType: 'mainModal',
         component: this.modalMain.approveComponent,
         folderName: this.modalMain.folderName
       })
-
-    },
-
-    /**
-     * receives the selected itemContext from btn-confirm component
-     * to be deleted & fetch DataContext again to update everything
-     *
-     * @param {Object} itemSelected
-     */
-    async confirmedActionAproveItemContext ({ itemSelected }) {
-      try {
-        await this.aproveItemContext(itemSelected.id)
-        this.$toast.success(this.$SUCCESS[this.messageToast.baseName].DELETED)
-
-        await this.getDataContext()
-
-        let currentPage = this.$store.state[this.modalMain.storeBase].currentPageOnTable
-
-        // if number of pages is minor that the current page, (when delete)
-        if (this.dataContext.length / ROWS_PER_PAGE_ON_TABLE <= (currentPage - 1)) {
-          currentPage--
-        }
-        // setting currentPage before to submit deleteItemContext and getDataContext
-        // to set it again as currentPage to prevent go to page 1 when fetch the dataContext
-        this.$store.commit(`${this.modalMain.storeBase}/${SET_CURRENT_PAGE_ON_TABLE}`, currentPage)
-      }
-      catch (e) {}
     }
   },
 
